ff640b25619e26b21240b785b727bb297e63859e
MartyLake <matthieu@talbot.audio>
2020-06-07 01:36:27 +0200

    Build and compile on windows.
    TODO:
    * remove hardcoded paths, see cmakelists
    * add install step for our example and cpack a msis installer

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "nifti2_io.h"
 
 int main() {
   std::cout << "Hello World!";
+  const auto nim = nifti_image_read("this_file_does_not_exist.nii", 1);
+  if (nim)
+    nifti_image_free(nim);
   return 0;
 }
